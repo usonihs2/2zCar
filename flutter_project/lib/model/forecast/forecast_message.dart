247af4464568f7c4570d639bb7d0129b4c3bca34
usonihs <godqhrdhtn@nate.com>
2023-01-11 10:39:10 +0900

    result screen 추가,  forecast tabbar, detail screen 수정

---
+++ flutter_project/lib/model/forecast/forecast_message.dart
@@ -0,0 +1,15 @@
+class ForecastResult {
+  static String year = 'select year';
+  static String odometer = '';
+  static String cylinders = '';
+  static String fuel = '';
+  static String drive = '';
+  static String size = '';
+
+  static String cylindersName = 'select cylinders';
+  static String fuelName = 'select fuel';
+  static String driveName = 'select drive';
+  static String sizeName = 'select size';
+
+  static String forecastResult = '';
+}
